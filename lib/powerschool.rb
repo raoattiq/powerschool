590e17633facb76b94f59b94503a7db78161ba46
harut <hharutyunyan111@gmail.com>
2019-07-25 14:11:09 +0400

    added new extension for student

--- lib/powerschool.rb
+++ lib/powerschool.rb
@@ -103,7 +103,7 @@ class Powerschool
   get :student, :ws, '/student/:student_id'
   get :students, :ws, '/student'
   get :school_teachers, :ws, '/school/:school_id/staff?expansions=emails,phones'
-  get :school_students, :ws, '/school/:school_id/student?expansions=addresses,phones,contact,contact_info,school_enrollment'
+  get :school_students, :ws, '/school/:school_id/student?expansions=addresses,phones,contact,contact_info,school_enrollment&extensions=u_studentsuserfields,studentcorefields'
   get :school_sections, :ws, '/school/:school_id/section'
   get :school_courses, :ws, '/school/:school_id/course'
   get :school_terms, :ws, '/school/:school_id/term'
